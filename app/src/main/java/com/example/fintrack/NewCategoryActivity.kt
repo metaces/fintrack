bb1409c17fe94fd851d36ff2914ffe1b4bb31ff0
cesaralberto <metaces@gmail.com>
2024-05-19 16:34:13 -0300

    criar nova categoria - spinner de cores custom spinner adapter para cores

--- app/src/main/java/com/example/fintrack/NewCategoryActivity.kt
+++ app/src/main/java/com/example/fintrack/NewCategoryActivity.kt
@@ -1,6 +1,11 @@
 package com.example.fintrack
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +15,23 @@ class NewCategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_category)
+
+        val colorSpinner = findViewById<Spinner>(R.id.spn_color)
+        val colorList = resources.getStringArray(R.array.color_name_array).toList()
+
+        val adapter = ColorSpinnerAdapter(this, colorList)
+        colorSpinner.adapter = adapter
+
+        colorSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parentView: AdapterView<*>, selectedItemView: View, position: Int, id: Long) {
+                val selectedColor = parentView.getItemAtPosition(position).toString()
+                Toast.makeText(this@NewCategoryActivity, "Selected Color: $selectedColor", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parentView: AdapterView<*>) {
+                // Do nothing
+            }
+        }
 
     }
 }
